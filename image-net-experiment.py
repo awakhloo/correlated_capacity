9752c8c79a8a26616af9cd10d99d52a4e22643d0
awakhloo <ajwakhloo@gmail.com>
2023-02-13 11:24:56 -0500

    added mean field cap to imgnet

--- image-net-experiment.py
+++ image-net-experiment.py
@@ -22,6 +22,7 @@ import capacity.replica_correlations as rep
 import capacity.manifold_simcap_analysis as num
 import capacity.low_rank_capacity as ori
 import capacity.utils.make_manifold_data as md 
+import capacity.mean_field_cap as mf
 import argparse
 
 raw_outdir = os.getcwd() + '/results/simclr/layers/raw'  # where to save raw representations
@@ -128,6 +129,7 @@ layers = glob(proj_outdir + f'/rep_{samp}/*.npy')
 np.random.seed(878668397)
 seeds = np.random.randint(low=1, high=1_000_000,size=(num_samp_reps, len(layers), 2))
 print(seeds)
+n_t, kappa, n_rep = 150, 0, 20 # number of gaussian vectors for alphas, the margin, and the number of label configs to try in simcap 
 for i, layer in enumerate(layers): 
     np.random.seed(seeds[samp, i, 0])
     name = os.path.relpath(layer, proj_outdir + f'/rep_{samp}')
@@ -136,14 +138,18 @@ for i, layer in enumerate(layers):
     mfs = np.load(layer, allow_pickle=True)
     mfs = [m for m in mfs]
     print(f'Shape check: {mfs[0].shape}')
-    alpha_ncom, *_ = ori.manifold_analysis_corr(mfs, 0, 150)
+    alpha_ncom, *_ = ori.manifold_analysis_corr(mfs, kappa, n_t)
     print(f'n. comms: {1/np.mean(1/alpha_ncom)}', flush = True)
     res_dct = {}
     res_dct['alpha_ncom'] = alpha_ncom
-    alpha_c, *_ = rep.manifold_analysis_corr(mfs, 0, 150)
+    alpha_c, *_ = rep.manifold_analysis_corr(mfs, kappa, n_t)
     res_dct['alpha_c'] = alpha_c
     print(f'theory: {alpha_c}', flush=True)
-    alpha_sim, P, Nc0, N_vec, p_vec = num.manifold_simcap_analysis(mfs, n_rep=20, seed=seeds[samp, i, 1])
+    alpha_sim, P, Nc0, N_vec, p_vec = num.manifold_simcap_analysis(mfs, n_rep=n_rep, seed=seeds[samp, i, 1])
     res_dct['alpha_sim'] = alpha_sim
     print(f'simulation: {alpha_sim}', flush = True)
+    alpha_mf, *_ = mf.manifold_analysis_corr(mfs, n_t=n_t, kappa=kappa)
+    alpha_mf = 1/np.mean(1/alpha_mf)
+    res_dct['alpha_m'] = alpha_mf 
+    print(f'mean field: {alpha_mf}', flush=True)
     np.save(outdir + f'/rep_{samp}/{name}_results.npy', np.array(res_dct))
