75b29c1381182c834912e6b0967696b32d5f4eda
awakhloo <ajwakhloo@gmail.com>
2023-02-15 14:05:31 -0500

    fix max_class specification in manifold data utils

--- capacity/utils/make_manifold_data.py
+++ capacity/utils/make_manifold_data.py
@@ -7,7 +7,7 @@ import torch
 from torchvision.models.feature_extraction import create_feature_extractor
 from torch.utils.data import Subset
 
-def make_manifold_data(dataset, sampled_classes, examples_per_class, max_class=None, seed=0):
+def make_manifold_data(dataset, sampled_classes, examples_per_class, seed, max_class=None):
     '''
     Samples manifold data for use in later analysis
 
@@ -17,23 +17,25 @@ def make_manifold_data(dataset, sampled_classes, examples_per_class, max_class=N
             the number of classes in dataset)
         examples_per_class: Number of examples per class to draw (there should be at least
             this many examples per class in the dataset)
-        max_class (optional): Maximum class to sample from. Defaults to sampled_classes if unspecified
+        max_class (optional): Maximum class to sample from. Defaults to randomly choosing from all unique IDs. 
         seed (optional): Random seed used for drawing samples
 
     Returns:
         data: list containing tensors for each class of shape (examples_per_class, input dimensions)
     '''
-    if max_class is None:
-        max_class = sampled_classes
     assert sampled_classes <= max_class, 'Not enough classes in the dataset'
-    assert examples_per_class * max_class <= len(dataset), 'Not enough examples per class in dataset'
+    assert examples_per_class * sampled_classes <= len(dataset), 'Not enough examples per class in dataset'
 
     # Set the seed
     np.random.seed(seed)
     # Storage for samples
     sampled_data = defaultdict(list)
     # Sample the labels
-    sampled_labels = np.random.choice(list(range(max_class)), size=sampled_classes, replace=False)
+    if max_class is None:
+        idxs = [dataset.class_to_idx[l] for l in dataset.classes]
+        sampled_labels = np.random.choice(idxs, size=sampled_classes, replace=False)
+    else:
+        sampled_labels = np.random.choice(list(range(max_class)), size=sampled_classes, replace=False)
     # Shuffle the order to iterate through the dataset
     idx = [i for i in range(len(dataset))]
     np.random.shuffle(idx)
